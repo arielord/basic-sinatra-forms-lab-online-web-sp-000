a2369cfef39de9f8e8dfeefdf34e94b63f1cb05b
Ariel <arielord@buffalo.edu>
2019-03-28 03:24:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ class App < Sinatra::Base
     erb :newteam
   end
   post '/team' do
+    params[:coach]
+  params[:point_guard]
+  <h2>Shooting Guard:<%= params[:shooting_guard]
+  <h2>Small Forward:<%= params[:small_forward]
+  <h2>Power Forward:<%= params[:power_forward]
+  <h2>Center<%= params[:center]
     erb :team
   end
 
